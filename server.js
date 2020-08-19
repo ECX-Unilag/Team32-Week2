2479f4dae9b3378199147621d2b35196fb459609
udofia2 <enalsy22@gmail.com>
2020-08-19 04:22:18 +0100

    removed unwanted files

--- server.js
+++ server.js
@@ -34,16 +34,6 @@ app.use("/api/v1/auth", require("./routes/api/auth"));
 app.use("/api/v1/profile", require("./routes/api/profile"));
 app.use("/api/v1/transaction", require("./routes/api/transactions"));
 
-//Serve Static assets in prod
-
-if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 app.listen(PORT, () =>
   console.log(`Server Started on port ${PORT} `)
 );
